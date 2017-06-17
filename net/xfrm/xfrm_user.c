ad8b506b88cbeb4217230d74f9f56240870c62ec
Amakura Hana <herlambangdicky5@gmail.com>
2017-06-17 07:09:29 +0700

    Revert "xfrm_user: validate XFRM_MSG_NEWAE XFRMA_REPLAY_ESN_VAL replay_window"
    This reverts commit 74a14acc87364c4000284e475f25a55e89e8dc83.

--- net/xfrm/xfrm_user.c
+++ net/xfrm/xfrm_user.c
@@ -380,9 +380,6 @@ static inline int xfrm_replay_verify_len(struct xfrm_replay_state_esn *replay_es
 			xfrm_replay_state_esn_len(up))
 		return -EINVAL;
 
-	if (up->replay_window > up->bmp_len * sizeof(__u32) * 8)
-		return -EINVAL;
-
 	return 0;
 }
 
